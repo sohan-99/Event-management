0a2b17727b658ef1ee584573773ac8a4dd0c18fe
sohan-99 <sohan75632@gmail.com>
2023-10-08 01:30:04 +0600

    about part are written some wedding when we are in your plan partner

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,7 +4,86 @@ const About = () => {
     return (
         <div>
             <Navber></Navber>
-            <h2>About</h2>
+            <h1 className="text-6xl font-bold text-center">About <span className="text-red-600">Us !</span> </h1>
+            <div className="flex space-x-5 my-10">
+                <div>
+                    <h2 className="text-lg font-bold text-red-400">
+                        Who We Are
+                    </h2>
+                    <p className="text-green-600 text-5xl font-semibold mb-6">
+                        Planner for Your <br />
+                        Perfect Wedding
+                    </p>
+                    <p className="text-xl font-medium">
+                        Vision & Budget, Timeline, Venue, Guest List,Wedding Party,<br />
+                        Date & Save the Date, Vendors,Attire, Décor & Theme, <br />
+                        Ceremony & Reception, Entertainment,  Invitations, Registry,<br />Transportation & Accommodation, Hair & Makeup, Rehearsal Dinner, <br />
+                        Marriage License, Wedding Day Timeline,Final Details, Emergency Kit,<br /> Relax & Enjoy.
+                    </p>
+                    <div className="stats bg-slate-400 shadow mt-36">
+
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            </div>
+                            <div className="stat-value">1565+</div>
+                            <div className="stat-title">Happy Couples</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+                            </div>
+                            <div className="stat-value">1200+</div>
+                            <div className="stat-title">Weddings</div>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-figure text-secondary">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                            </div>
+                            <div className="stat-value">879+</div>
+                            <div className="stat-title">Decoration</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[500px]">
+                    <img className="w-[450px] h-[450]" src="https://i.ibb.co/m8FxyMM/817a781feaafd6ea2ba6d896f186c087.jpg" alt="" />
+                </div>
+            </div>
+            {/* carusol */}
+            {/* <div className="carousel w-[1100px] h-[580px]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/JdgLgGd/Alekhya-Himanth-Wedding-459.webp" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/gjjFY0T/wedding-planners-singapore-ali-salah-pexels.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/T8wtwd7/140115-41-a-bride-and-groom-exiting-their-wedding-ceremony-as-guests-throw-white-confetti-over-them.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/Db3xswZ/Beth-Brad-17-jpg.webp" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div> */}
+
+
+
+
         </div>
     );
 };
--- src/pages/Allcourse/Allcourse.jsx
+++ src/pages/Allcourse/Allcourse.jsx
@@ -6,20 +6,29 @@ import Navber from "../Share/Navber/Navber";
 import { Link } from "react-router-dom";
 
 const Allcourse = () => {
-    const [deta,setDeta]=useState([]);
+    const [deta, setDeta] = useState([]);
 
-    useEffect(()=>{
-        fetch('course.json ')
-        .then(res=>res.json())
-        .then(data=> setDeta(data))
-            },[])
+    useEffect(() => {
+        fetch('event.json ')
+            .then(res => res.json())
+            .then(data => setDeta(data))
+    }, [])
     return (
         <div classNameName="">
             <Navber></Navber>
-           <h1 className="text-6xl font-bold text-center">Our <span className="text-red-600">Servies</span> </h1>
+            <h1 className="text-6xl font-bold text-center">Our <span className="text-red-600">Servies</span> </h1>
             <h2 className="lg:text-4xl md:text-2xl text-xl  lg:font-semibold md:font-medium font-normal text-center my-4"><span className="text-blue-600">All Services : </span>{deta.length}</h2>
             {
-                deta.map(title=><Link key={title.id} className="block text-center rounded-xl  lg:text-3xl md:text-xl text-lg p-6  lg:font-medium md:font-normal font-light">{title.courseName}</Link>)
+                deta.map(title => <Link
+                    key={title.id} className="  text-center rounded-xl  lg:text-3xl md:text-xl text-lg p-6  lg:font-medium md:font-normal font-light">
+                    <div className="">
+
+                        <button
+                            className="btn btn-primary text-center">{title.id}:{title.eventName}
+                        </button>
+                        <img className="w-96 object-center" src={title.image} alt="" />
+                    </div>
+                </Link>)
             }
         </div>
     );
